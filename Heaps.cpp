27b0b7f58064c9e1394494c971392c1e5d2f2671
callisto-h <hesscallisto@gmail.com>
2022-11-27 14:33:23 -0500

    review material concerning Heaps and Hash Tables

---
+++ Heaps.cpp
@@ -0,0 +1,61 @@
+
+
+/**
+ * QUESTIONS:
+ * 1. What type of tree is required to implement a heap?
+ *    Why does it matter? (For what operations to function
+ *    do we need that type of tree)
+ * 
+ * 2. How are BSTs and Heaps similar? How are they different?
+ * 
+ * 3. What is the height of a Heap with N nodes in it?
+ * 
+ * 4. Describe the process of inserting a new value into a max-heap
+ * 
+ * 5. What is the runtime complexity of insert()? Why?
+ *    
+ *    BONUS: Can you think of a case when insert() might be
+ *           especially slow? Think Dynamic Arrays... 
+ * 
+ * 6. Describe the process of removing the top value from a max-heap
+ * 
+ * 7. What is the runtime complexity of remove()? Why?
+ * 
+ * 8. Describe the process of building a max-heap from an array of
+ *    unordered values
+ * 
+ * 9. What is the runtime complexity of build-max-heap()? Why?
+ * 
+ */
+
+/**
+ * QUESTIONS:
+ * 
+ * 10. Describe the heapsort algorithm
+ * 
+ * 11. What is the runtime complexity of heapsort()?
+ * 
+ * 12. What is the runtime complexity of calling build-max-heap()
+ *     and then calling heapsort() on the max-heap?
+ */
+
+/**
+ * QUESTIONS:
+ * 
+ * 13. Given the following heap, remove once and then insert the value
+ *     8 once.
+ * 
+                   ┌─┐
+                   │9│
+              ┌────┴─┴────┐
+              │           │
+             ┌▼┐         ┌▼┐
+             │7│         │5│
+         ┌───┴─┴──┐    ┌─┴─┘
+         │        │    │
+        ┌▼┐      ┌▼┐  ┌▼┐
+        │1│      │4│  │2│
+        └─┘      └─┘  └─┘
+
+ *
+ */
